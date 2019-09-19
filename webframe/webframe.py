79945c0ab2806a51a79f9e58565ed404c00dc307
Seal <seal.xu@qq.com>
2019-09-19 14:34:28 +0800

    add webframe.py, setting.py,static/

---
+++ webframe/webframe.py
@@ -0,0 +1,24 @@
+"""
+webframe
+模拟网站后端应用行为
+
+从httpserver接收具体请求
+根据请求进行逻辑处理和数据处理
+将需要的数据反馈给httpserver
+"""
+
+from socket import *
+import json
+from day9_19.webframe.settings import *
+from threading import Thread
+from multiprocessing import Process
+
+class Application:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+if __name__ == '__main__':
+    pass
